332a2abef8aa1e50822b5c123a10a5dcdc0a68a8
alok <trickyguruji@gmail.com>
2022-09-01 18:59:15 +0530

    day 1 changing

---
+++ Day1/Variable.java
@@ -0,0 +1,10 @@
+public class Variable {
+    public static void main(String[] args) {
+        int myFirstNumer=6;
+        String boy="my name is alok kumar";
+        System.out.println(myFirstNumer);
+        System.out.println(10+5);
+        System.out.println(boy);
+    }
+    
+}
